957be13738bbfa5ae6b55a8ba3b6da915839d1b7
Tair <trouzerop@gmail.com>
2025-09-10 11:52:13 +0600

    run.go in the kitchen

--- internal/kitchen/run.go
+++ internal/kitchen/run.go
@@ -1,14 +1,16 @@
-package kitchen 
+package kitchen
 
 import (
-	"os/signal"
-	"syscall"
 	"context"
 	"flag"
-	"wheres-my-pizza/internal/xpkg/logger"
-	"wheres-my-pizza/internal/xpkg/config"
+	"fmt"
+	"os/signal"
+	"sort"
+	"strings"
+	"syscall"
 	"wheres-my-pizza/internal/kitchen/app/core"
-	"wheres-my-pizza/internal/kitchen/aggregator/worker"
+	"wheres-my-pizza/internal/xpkg/config"
+	"wheres-my-pizza/internal/xpkg/logger"
 )
 
 type params struct {
@@ -37,7 +39,7 @@ func Execute(ctx context.Context, mylog logger.Logger, args []string) error {
 
 	worker := workers.NewWorker(newCtx, cancel, context.Background(), params.cfg, params.workerParams, mylog)
 
-	if err := worker.Run(); err != nil{
+	if err := worker.Run(); err != nil {
 		mylog.Action("consumer_failed").Error("Error running consumer", err)
 		return err
 	}
@@ -55,10 +57,73 @@ func parseParams(args []string) (*params, error) {
 	configPath := fs.String("config-path", "config.yaml", "path for config yaml")
 
 	workerName := fs.String("worker-name", "", "Max concurrent requests")
+	orderTypes := fs.String("order-types", "", "Optional. Comma-separated list of order types the worker can handle (e.g., dine_in, takeout). If omitted, handles all.")
+	heartbeatInterval := fs.Int("heartbeat-interval", 30, "Interval (seconds) between heartbeats")
+	prefetch := fs.Int("prefetch", 1, "RabbitMQ prefetch count, limiting how many messages the worker receives at once")
 
+	if err := fs.Parse(args); err != nil {
+		return nil, fmt.Errorf("cannot parse: %w", err)
+	}
+
+	if *showHelp {
+		fs.Usage()
+		return nil, core.ErrHelp
+	}
+
+	processedOrderTypes := strings.Split(*orderTypes, ",")
+
+	if len(processedOrderTypes) == 1 && processedOrderTypes[0] == "" {
+		processedOrderTypes = []string{}
+		for orderType := range core.AllowedOrderTypes {
+			processedOrderTypes = append(processedOrderTypes, orderType)
+		}
+		sort.Strings(processedOrderTypes)
+	}
+
+	return &params{
+		workerParams: &core.WorkerParams{
+			WorkerName:        *workerName,
+			OrderTypes:        processedOrderTypes,
+			HeartbeatInterval: *heartbeatInterval,
+			Prefetch:          *prefetch,
+		},
+		configPath: *configPath,
+	}, nil
 }
 
 // validateParams validates params
 func validateParams(params *params) error {
+	cfg, err := config.LoadConfig(params.configPath)
+	if err != nil {
+		return err
+	}
+	params.cfg = cfg
 
+	workerParams := params.workerParams
+
+	if workerParams.WorkerName == "" {
+		return fmt.Errorf("worker name is required: %s", workerParams.WorkerName)
+	}
+
+	if len(workerParams.OrderTypes) > 0 {
+		validateOrderTypes := make([]string, 0)
+		for _, orderType := range workerParams.OrderTypes {
+			orderType = strings.TrimSpace(orderType)
+			if !core.AllowedOrderTypes[orderType]{
+				return fmt.Errorf("unknown order type: %s", orderType)
+			}
+			validateOrderTypes = append(validateOrderTypes, orderType)
+		}
+		workerParams.OrderTypes = validateOrderTypes
+	}
+
+	if workerParams.HeartbeatInterval <= {
+		return fmt.Errorf("heartbeat-interval cannot be less or equal zero: %d", workerParams.HeartbeatInterval)
+	}
+
+	if workerParams.Prefetch <= {
+		return fmt.Errorf("prefetch cannot be less or equal zero: %d", workerParams.Prefetch)
+	}
+
+	return nil
 }
